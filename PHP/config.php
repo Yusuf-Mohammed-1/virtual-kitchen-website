4ebf3343ed80f2425da6697c262ed2cda60a5223
Yusuf-Mohammed-1 <ymohammed0100@gmail.com>
2025-09-28 16:04:12 +0100

    Update config.php

--- PHP/config.php
+++ PHP/config.php
@@ -3,12 +3,16 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-
+// Database credentials (replace with your own when running locally)
 $servername = "localhost";            
-$username = "u-240104887";           
-$password = "IlLftOhKxuY4Gad";         
-$dbname = "u_240104887_db";            
-
+$username = "your_username";           
+$password = "your_password";         
+$dbname = "your_database_name";            
 
 $conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
